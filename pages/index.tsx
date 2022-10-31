3a87bc42f67ca3b8e01b41390db6931a9d43025d
iueu <ilwelisaac@hotmail.com>
2022-10-30 21:54:47 -0300

    feat: Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react"
+import styled from "styled-components"
+import useUser from "../hooks/useUser"
+import User from "../types/User"
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`
+
 export default function Home() {
-  return (
-    <div className="dark">
-      Hello
-    </div>
-  )
+  const user = useUser()
+
+  if(user){
+      return (
+        <Container>
+          <h1>Bem Vindo {user.username} ({user.nickname})</h1>
+        </Container>
+      )
+  }
+
+  
 }
