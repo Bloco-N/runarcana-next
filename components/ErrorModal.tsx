4151f765d4b4a3a155eecb13154dec2490f9e568
iueu <ilwelisaac@hotmail.com>
2022-10-30 21:50:37 -0300

    feat: error modal

---
+++ components/ErrorModal.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 12rem;
+  padding: 4rem;
+  color: var(--error);
+`
+type ErrorModalProps = {
+  message: string
+}
+
+const ErrorModal = (props:ErrorModalProps) => {
+  return (
+    <Container>
+      <p>{props.message}</p>
+    </Container>
+  );
+};
+
+export default ErrorModal;
