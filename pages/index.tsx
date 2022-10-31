712c3d98fafc5f0c40c115d376d946b74f10b4e2
iueu <ilwelisaac@hotmail.com>
2022-10-31 19:48:57 -0300

    feat: change Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react"
 import styled from "styled-components"
+import Button from "../components/Button"
+import Card from "../components/Card"
 import useUser from "../hooks/useUser"
-import User from "../types/User"
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   height: 100%;
-  align-items: center;
-  justify-content: center;
+  padding: 10rem;
+
+  h1{
+    margin-bottom: 5rem;
+  }
+  .c-characters{
+    display: flex;
+    gap: 2rem;
+    margin-top: 4rem;
+    div{
+      height: 20rem;
+      width: 20rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+  button{
+    border-radius: 50%;
+    height: 2rem;
+    width: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `
 
 export default function Home() {
@@ -17,6 +42,15 @@ export default function Home() {
       return (
         <Container>
           <h1>Bem Vindo {user.username} ({user.nickname})</h1>
+
+          <h2>Personagens</h2>
+          <div className='c-characters'>
+            <Card>
+              <Button>+</Button>
+            </Card>
+          </div>
+
+
         </Container>
       )
   }
