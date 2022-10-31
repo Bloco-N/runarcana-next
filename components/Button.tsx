8a577f223afe83be073335c836f03f1deae45388
iueu <ilwelisaac@hotmail.com>
2022-10-31 19:47:37 -0300

    feat: change button opacity

--- components/Button.tsx
+++ components/Button.tsx
@@ -16,7 +16,7 @@ const Container = styled.button<ContainerProps>`
   background: ${props => props.isDark ? 'rgba(0, 9, 24, 0.25)' :'rgba(245, 207, 240, 0.25)' } ;
   backdrop-filter: blur(0.2rem);
   border: 0.4rem outset ${props => props.isDark ? 'rgba(40, 52, 73, 0.7)' :'rgba(236, 111, 220, 0.25)' };
-  opacity: 0.7;
+  opacity: 0.4;
   font-size: 2rem;
   font-weight: bold;
   cursor: pointer;
