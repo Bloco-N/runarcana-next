a6a36956ef40996f491da6f87145e02582b98039
iueu <ilwelisaac@hotmail.com>
2022-11-12 20:06:19 -0300

    feat: exclude character modal

---
+++ components/ConfirmExcludeCharacterModal.tsx
@@ -0,0 +1,66 @@
+import { useMutation } from "@apollo/client"
+import { useRouter } from "next/router";
+import React, { Dispatch, SetStateAction } from 'react';
+import styled from 'styled-components';
+import { DELETE_CHARACTER } from "../gql/mutations";
+import Button from './Button';
+import Card from './Card';
+
+type ConfirmModalProps = {
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+  characterId: number
+}
+
+const Container = styled.div`
+  background-color: #000000b5;
+  position: absolute;
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  height: 80%;
+  .content{
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    .buttons{
+      display: flex;
+      gap: 2rem;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+`
+
+const ConfirmExcludeCharacterModal = (props:ConfirmModalProps) => {
+  const router = useRouter()
+  const [mutateFunction] = useMutation(DELETE_CHARACTER, {errorPolicy:'all'})
+  const handleDelete = () => {
+    const token = localStorage.getItem('token')
+    mutateFunction({
+      variables:{
+        deleteCharacterId: props.characterId
+      },
+      context:{
+        headers:{
+          authorization: 'Bearer ' + token
+        }
+      }
+    })
+    router.reload()
+  }
+  return (
+    <Container>
+      <Card className='content'>
+          <h2>Deseja realmente excluir esse personagem do codex?</h2>
+          <div className="buttons">
+            <Button onClick={handleDelete}>Sim</Button>
+            <Button onClick={() => props.setIsOpen(false)}>Não</Button>
+          </div>
+      </Card>
+    </Container>
+  );
+};
+
+export default ConfirmExcludeCharacterModal;
